b97147a26aa6c5baaba162efe6df4ecb9c558d55
EduardaGoulart <goularteduda97@gmail.com>
2018-05-30 08:03:57 -0300

    fase 3

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+
+    public float speed = 0;
+    private Material mat;
+    private GameObject p1;
+    private Vector2 move = Vector2.zero;
+
+    float pos = 0;
+
+    void Start()
+    {
+
+        mat = GetComponent<Renderer>().material;
+        p1 = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = p1.GetComponent<Rigidbody2D>().velocity.x;
+        move.x = Input.GetAxis("Horizontal");
+
+        if (vel != 0f)
+        {
+            var side = move.x;
+            pos += speed * side;
+            mat.mainTextureOffset = new Vector2(pos, 0);
+        }
+    }
+}
